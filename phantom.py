9a662395cb0125db74957c556b4123ee80d1769c
eytan <edaniyalzade@walmartlabs.com>
2018-02-05 10:37:42 -0500

    start returning structured data from the current account bill

--- phantom.py
+++ phantom.py
@@ -1,3 +1,4 @@
+import re
 from time import sleep
 
 from bs4 import BeautifulSoup
@@ -9,10 +10,11 @@ from utils import parse_price
 DEBUG_FILE = 'tmp/coned.html'
 IMAGE_FILE = 'tmp/screen.png'
 LOGIN_URL = 'https://www.coned.com/en/login'
+REGEX_DATE = r'(\d+/\d+/\d+)'
+REGEX_AMOUNT = r'\$(\d+.\d+)'
 
 
-def _get_account_page(username, password, options=None):
-    options = options or {}
+def _get_account_page(username, password, debug=False):
     driver = webdriver.PhantomJS()
     driver.set_window_size(1024, 768)
     driver.get(LOGIN_URL)
@@ -25,22 +27,43 @@ def _get_account_page(username, password, options=None):
     print 'sleeping to get the new page'
     sleep(10)
     html_source = driver.page_source.encode('utf8')
-    if options.get('debug'):
+    if debug:
         open(DEBUG_FILE, "w").write(html_source)
         driver.save_screenshot(IMAGE_FILE)
     return html_source
 
 
-def _parse_currend_bill(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    return parse_price(soup.find(id='divAccountBalance').find('b').text)
+def _parse_bill_text(text):
+    due = re.search(REGEX_DATE, text).group(1)
+    ammount = parse_price(re.search(REGEX_AMOUNT, text).group(1))
+    return (due, ammount)
 
+
+def _parse_current_bill(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    prev, cur = soup.find(id='divAccountBalance').text.lower().split('current')
+    prev_due, prev_ammount = _parse_bill_text(prev)
+    cur_due, cur_ammount = _parse_bill_text(cur)
+    return [{
+            'ammount': prev_ammount,
+            'due': prev_due,
+            'current': False,
+            },
+            {
+            'ammount': cur_ammount,
+            'due': cur_due,
+            'current': True,
+            }]
 
 p = configargparse.ArgParser(default_config_files=['.credentials'])
 p.add('--username', required=True, help='username')
 p.add('--password', required=True, help='password')
+p.add('--debug', help='debug', action='store_true')
 
 options = p.parse_args()
-html = _get_account_page(options.username, options.password)
-print _parse_currend_bill(html)
-# print _parse_currend_bill(open(DEBUG_FILE).read())
+html = _get_account_page(options.username, options.password,
+        debug=options.debug
+        )
+print _parse_current_bill(html)
+
+# print _parse_current_bill(open(DEBUG_FILE).read())
