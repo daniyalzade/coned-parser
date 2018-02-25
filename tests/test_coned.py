814fa54b5dc9578eef6c2865f26a9060240cee65
eytan <edaniyalzade@walmartlabs.com>
2018-02-25 12:28:28 -0500

    add more test cases for coned

--- tests/test_coned.py
+++ tests/test_coned.py
@@ -2,8 +2,7 @@ import os
 
 import coned
 
-PATH = os.path.join(os.path.dirname(__file__), './assets/coned.html')
-HTML = open(PATH).read()
+cur_dir = os.path.dirname(__file__)
 
 EXPECT = [
     {'current': False, 'amount': 165.29, 'due': u'02/14/2018'},
@@ -11,4 +10,9 @@ EXPECT = [
 ]
 
 def test_parse_current_bill():
-    assert EXPECT == coned._parse_current_bill(HTML), "correctly parsed account"
+    html = open(os.path.join(cur_dir, './assets/coned.html')).read()
+    assert EXPECT == coned._parse_current_bill(html), "correctly parsed account"
+
+def test_parse_bills():
+    html = open(os.path.join(cur_dir, './assets/coned_bills.html')).read()
+    assert 18 == len(coned._parse_bills(html)), "correctly parsed account"
